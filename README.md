abc66d0ba12cbffe0d4af84bf73203b36d1bf4bf
SteffenGoerzig <83447776+SteffenGoerzig@users.noreply.github.com>
2021-04-30 17:57:42 +0200

    Initial check in.

---
+++ README.md
@@ -0,0 +1,54 @@
+# roadC
+Read-Only Array Data Compaction (roadC)
+
+Standard data compression techniques cannot be applied for traditional embedded systems since these systems are typically too restricted by timing constraints or by memory limitations. 
+
+Read-Only Array Data Compaction (roadC) addresses this problem: it is an approach for compression of read-only array data. The main advantage over standard approaches is the ability to access compressed data without any decompression, i.e. without any extra computational and memory costs.
+
+Memory handling of CPUs can require special demands on the way data is stored in the memory. In this context roadC can handle data alignment, data structure padding, and memory protection. 
+
+The core functionality of roadC is closely related the shortest common superstring problem (SCS): to find the shortest possible string that contains every string in a given set as substrings.
+
+The roadC library is a reference implementation of the roadC algorithm. It is implemented in C and can be compiled using a C or C++ compiler.
+
+## Quickstart Guide
+
+1. Get the roadC library and type
+
+    ```bash
+    make all
+    ```
+
+2. Run the example programs, e.g.
+
+    ```bash
+    roadc-example1
+    ```
+
+**Happy coding!**
+
+## Requirements
+
+* gcc compiler
+
+## Contributing
+
+We welcome any contributions.  
+If you want to contribute to this project, please read the [contributing guide](CONTRIBUTING.md).  
+To get started with the development, please read the documentation.
+
+## Code of Conduct
+
+Please read our [Code of Conduct](CODE_OF_CONDUCT.md) as it is our base for interaction.
+
+## License
+
+This project is licensed under the [MIT LICENSE](LICENSE.md).
+
+## Provider Information
+
+Please visit <https://github.com/Daimler/daimler-foss/blob/master/LEGAL_IMPRINT.md> for information on the provider.
+
+Notice: Before you use the program in productive use, please take all necessary precautions,
+e.g. testing and verifying the program with regard to your specific use.
+The program was tested solely for our own use cases, which might differ from yours.
---
+++ src/roadc-example3.c
@@ -0,0 +1,92 @@
+/*
+The MIT License
+
+Copyright (c) 2021 MBition GmbH
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/* SPDX-License-Identifier: MIT */
+
+/* roadC example 3 */
+
+/* printf() */
+#ifdef __cplusplus
+#include <cstdio>
+#else
+#include <stdio.h>
+#endif /* __cplusplus */
+
+#include "roadc.h"
+
+tRoadcByte arr1[4]={2, 3, 3, 4};
+tRoadcByte arr1Pad[4]={1, 2, 0, 0};
+tRoadcByte arr2[3]={2, 3, 3};
+tRoadcByte arr2Pad[3]={0, 255, 0};
+tRoadcByte arr3[2]={5, 7};
+tRoadcByte arr3Pad[2]={0, 252};
+tRoadcByte arr4[5]={1, 2, 3, 4, 5};
+tRoadcByte arr4Pad[5]={255, 255, 0, 0, 0};
+tRoadcByte arr5[3]={4, 5, 0};
+tRoadcByte arr5Pad[3]={0, 0, 255};
+
+int main(int arc, char **argv){
+  tRoadcPtr pRoadc;
+  tRoadcBytePtr compactedData;
+  tRoadcBytePtr compactedDataPaddingByteMask;
+  tRoadcUInt32 compactedDataSize;
+  tRoadcUInt32 compactedDataAlignment;
+  tRoadcUInt32 i;
+
+  pRoadc = roadcNew();
+
+  roadcAddElement(pRoadc, arr1, arr1Pad, 4, 1);
+  roadcAddElement(pRoadc, arr2, arr2Pad, 3, 4);
+  roadcAddElement(pRoadc, arr3, arr3Pad, 2, 8);
+  roadcAddElement(pRoadc, arr4, arr4Pad, 5, 4);
+  roadcAddElement(pRoadc, arr5, arr5Pad, 3, 2);
+
+  roadcCalculation(pRoadc, 1, 0);
+
+  compactedDataSize = roadcGetCompactedDataSize(pRoadc);
+  compactedDataAlignment = roadcGetCompactedDataAlignment(pRoadc);
+  compactedData = roadcGetCompactedData(pRoadc);
+  compactedDataPaddingByteMask = roadcGetCompactedDataPaddingByteMask(pRoadc);
+
+  printf("Compacted data size: %lu alignment: %lu\n", compactedDataSize, compactedDataAlignment);
+  printf("data: ");
+  for (i=0; i<compactedDataSize; i++){
+    printf("%3u ", compactedData[i]);
+  }
+  printf("\nmask: ");
+  for (i=0; i<compactedDataSize; i++){
+    printf("%3u ", compactedDataPaddingByteMask[i]);
+  }
+  printf("\n");
+
+  printf("arr1 position in compacted data: %lu \n", roadcGetPositionInCompactedData(pRoadc, arr1, arr1Pad, 4, 1));
+  printf("arr2 position in compacted data: %lu \n", roadcGetPositionInCompactedData(pRoadc, arr2, arr2Pad, 3, 4));
+  printf("arr3 position in compacted data: %lu \n", roadcGetPositionInCompactedData(pRoadc, arr3, arr3Pad, 2, 8));
+  printf("arr4 position in compacted data: %lu \n", roadcGetPositionInCompactedData(pRoadc, arr4, arr4Pad, 5, 4));
+  printf("arr5 position in compacted data: %lu \n", roadcGetPositionInCompactedData(pRoadc, arr5, arr5Pad, 3, 2));
+
+  roadcDelete(pRoadc);
+
+  return 0; 
+}
